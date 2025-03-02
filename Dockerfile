72cdf96e2e1fa58a22d6c86e088646875adc7f49
Phạm Dương <pduongpdu99@gmail.com>
2025-03-02 10:23:50 +0700

    update: main.py -> app.py

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+from python:3.12.3
+
+workdir /app
+
+copy . .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+ENV FLASK_APP=main.py
+ENV FLASK_RUN_HOST=0.0.0.0
+ENV FLASK_RUN_PORT=5000
+ENV WDS_SOCKET_PORT=0
+
+EXPOSE 5000
+
+cmd ["python", "main.py"]
+
---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify, render_template
+from process import pred as p
+from typing import Optional
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
+
+@app.route('/pred', methods=['POST', 'OPTIONS'])  # Handle preflight requests
+def pred():
+    if request.method == 'OPTIONS':  # Preflight request
+        response = jsonify({"message": "CORS preflight"})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS")
+        response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+        return response, 204  # No content response
+
+    data = request.get_json()
+    opt: Optional[str] = request.args.get('opt', None)
+    if not opt:
+        opt = 'trn'
+    opt = opt.upper()
+
+    nn: list[float] = data['nn']
+    
+    response = jsonify({"predict": p(nn, opt)[0]})
+    response.headers.add("Access-Control-Allow-Origin", "*")  # Explicitly allow cross-origin
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=False, host="0.0.0.0")
